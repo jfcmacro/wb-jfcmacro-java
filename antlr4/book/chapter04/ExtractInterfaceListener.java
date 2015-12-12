2006a88232791ab8ef5c733aa08c5dc668c37695
Juan Francisco Cardona McCormick <jfcmacro@gmail.com>
2015-12-12 13:02:38 +0000

    Adding ANTLR examples of Chapter 04

---
+++ antlr4/book/chapter04/ExtractInterfaceListener.java
@@ -0,0 +1,65 @@
+/***
+ * Excerpted from "The Definitive ANTLR 4 Reference",
+ * published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material, 
+ * courses, books, articles, and the like. Contact us if you are in doubt.
+ * We make no guarantees that this code is fit for any purpose. 
+ * Visit http://www.pragmaticprogrammer.com/titles/tpantlr2 for more book information.
+***/
+import org.antlr.v4.runtime.TokenStream;
+import org.antlr.v4.runtime.misc.Interval;
+import org.antlr.v4.runtime.tree.TerminalNode;
+import java.util.List;
+
+public class ExtractInterfaceListener extends JavaBaseListener {
+    JavaParser parser;
+    public ExtractInterfaceListener(JavaParser parser) {this.parser = parser;}
+    /** Listen to matches of classDeclaration */
+    @Override public void enterPackageDeclaration(JavaParser.PackageDeclarationContext ctx) {
+	System.out.print("package ");
+	List<TerminalNode> ids = ctx.qualifiedName().Identifier();
+	for (int i = 0; i < ids.size(); ++i) {
+	    System.out.print(ids.get(i).getText());
+	    if ((i + 1) != ids.size()) System.out.print('.');
+	}
+	System.out.println(';');
+    }
+
+    @Override public void exitPackageDeclaration(JavaParser.PackageDeclarationContext ctx) {
+    }
+
+    @Override public void enterImportDeclaration(JavaParser.ImportDeclarationContext ctx) {
+
+	TokenStream tokens = parser.getTokenStream();
+	System.out.println(tokens.getText(ctx));
+    }
+
+    @Override public void exitImportDeclaration(JavaParser.ImportDeclarationContext ctx) {
+    }
+    
+    @Override
+    public void enterClassDeclaration(JavaParser.ClassDeclarationContext ctx){
+        System.out.println("interface I"+ctx.Identifier()+" {");
+    }
+    
+    @Override
+    public void exitClassDeclaration(JavaParser.ClassDeclarationContext ctx) {
+        System.out.println("}");
+    }
+
+    /** Listen to matches of methodDeclaration */
+    @Override
+    public void enterMethodDeclaration(
+        JavaParser.MethodDeclarationContext ctx
+    )
+    {
+        // need parser to get tokens
+        TokenStream tokens = parser.getTokenStream();
+        String type = "void";
+        if ( ctx.type()!=null ) {
+            type = tokens.getText(ctx.type());
+        }
+        String args = tokens.getText(ctx.formalParameters());
+        System.out.println("\t"+type+" "+ctx.Identifier()+args+";");
+    }
+}
