251b3bdaef226ccb1a4e8812247b8804ec43a806
Juan Francisco Cardona McCormick <jfcmacro@gmail.com>
2018-08-05 23:29:59 +0000

    Updating wb-jfcmacro

---
+++ Hello3.java
@@ -0,0 +1,15 @@
+
+public class Hello3 {
+
+   public static void main(String args[]) {
+      String hello = "Hola\n";
+      String hello2 = "\110\157\154\141\40\12";
+      String hello3 = "\"\\no\"";
+      
+      // String hello3 = "\0x48\0x6F\0x6C\0x61\0x20\0x0A";
+
+      System.out.print(hello);
+      System.out.print(hello2);
+      System.out.print(hello3);
+   }
+}
