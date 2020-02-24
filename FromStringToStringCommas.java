2aeaa1a3a711385ee633444382e88d3ef9e630f7
Juan Francisco Cardona McCormick <jfcmacro@gmail.com>
2020-02-24 15:35:18 +0000

    Adding examples

--- FromStringToStringCommas.java
+++ FromStringToStringCommas.java
@@ -5,17 +5,18 @@ public class FromStringToStringCommas {
     public static void main(String []args) {
 
 	Scanner scan = new Scanner(System.in);
+	final int SIZE = 3;
 
 	System.out.print("Write down a number: ");
 	String input = scan.nextLine();
 	StringBuffer output = new StringBuffer();
 	int i = input.length();
 
-	for (; i > 3; i-=3)
-	    output.insert(0,input.substring((i-3), i)).insert(0,(((i - 3) == 0) ? "" : ',' ));
+	for (; i > SIZE; i-=SIZE)
+	    output.insert(0,input.substring((i-SIZE), i)).insert(0,(((i - SIZE) == 0) ? "" : ',' ));
 
-	if (i > 0)
-	    output.insert(0, input.substring(0, i));
+	// if (i > 0)
+	//     output.insert(0, input.substring(0, i));
 
 	System.out.println("Number with commas: " + output);
     }
