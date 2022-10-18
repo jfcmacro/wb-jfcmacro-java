decebfba0e3b8d7c52e1cc885a076a99d2f5b003
Juan Cardona-EPAM <Juan_Cardona@epam.com>
2022-10-18 15:56:20 +0000

    Updating 30-days-code

---
+++ 30-days-code/day00/Day00.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Day00 {
+    public static void main(String[] args) {
+	Scanner scan = new Scanner(System.in);
+	String msg = scan.nextLine();
+	System.out.println("Hello, World.");
+	System.out.println(msg);
+    }
+}
