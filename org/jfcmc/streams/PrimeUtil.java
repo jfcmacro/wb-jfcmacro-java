b90fc7f8367cf3134e494146d894dc4135d4613f
Juan Francisco Cardona McCormick <jfcmacro@gmail.com>
2022-10-19 13:14:28 +0000

    Adding a stream example PrimeUtil

---
+++ org/jfcmc/streams/PrimeUtil.java
@@ -0,0 +1,41 @@
+package org.jfcmc.streams;
+
+public class PrimeUtil {
+    private long lastPrime = 0L;
+
+    public long next() {
+	lastPrime = next(lastPrime);
+	return lastPrime;
+    }
+
+    public static long next(long after) {
+	long counter = after;
+
+	while (!isPrime(++counter));
+
+	return counter;
+    }
+
+    public static boolean isPrime(long number) {
+	if (number <= 1) {
+	    return false;
+	}
+
+	if (number == 2) {
+	    return true;
+	}
+
+	if (number % 2 == 0) {
+	    return false;
+	}
+
+	long maxDivisor = (long) Math.sqrt(number);
+	for (int counter = 3; counter <= maxDivisor; counter += 2) {
+	    if (number % counter == 0) {
+		return false;
+	    } 
+	}
+
+	return true;
+    }
+}
