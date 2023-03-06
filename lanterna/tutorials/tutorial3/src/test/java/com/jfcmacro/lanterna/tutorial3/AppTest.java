b11f286aa31a2eb9d08dff6c3f72b3431b918acb
Juan Cardona-EPAM <Juan_Cardona@epam.com>
2023-03-06 23:53:53 +0000

    Adding tutorial examples lanterna

---
+++ lanterna/tutorials/tutorial3/src/test/java/com/jfcmacro/lanterna/tutorial3/AppTest.java
@@ -0,0 +1,20 @@
+package com.jfcmacro.lanterna.tutorial3;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
