b3f69efa46f4769b0d6132898ba6ad66b24b2e62
Juan Cardona-EPAM <Juan_Cardona@epam.com>
2024-03-21 17:24:29 +0000

    Adding Profile and setting index

--- books/Servlets y JavaServer Pages/chap02/src/main/java/org/jfcmc/servlet/chap02/HolaWWW.java
+++ books/Servlets y JavaServer Pages/chap02/src/main/java/org/jfcmc/servlet/chap02/HolaWWW.java
@@ -10,20 +10,20 @@ import javax.servlet.http.HttpServletResponse;
 public class HolaWWW extends HttpServlet {
 
     public void doGet(HttpServletRequest request,
-		      HttpServletResponse response)
-	throws ServletException, IOException {
+                      HttpServletResponse response)
+        throws ServletException, IOException {
 
-	PrintWriter out = response.getWriter();
-	String docType =
-	    "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 " +
-	    "Transitional//EN\">\n";
+        PrintWriter out = response.getWriter();
+        String docType =
+            "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 " +
+            "Transitional//EN\">\n";
 
 
-	out.println(docType +
-		    "<HTML>\n" +
-		    "<HEAD><TITLE>Hola WWW</TITLE></HEAD>\n" +
-		    "<BODY>\n" +
-		    "<H1>Hola WWW</H1>\n" +
-		    "</BODY></HTML>");
+        out.println(docType +
+                    "<HTML>\n" +
+                    "<HEAD><TITLE>Hola WWW</TITLE></HEAD>\n" +
+                    "<BODY>\n" +
+                    "<H1>Hola WWW</H1>\n" +
+                    "</BODY></HTML>");
     }
 }
--- books/Servlets y JavaServer Pages/chap02/src/main/webapp/index.jsp
+++ books/Servlets y JavaServer Pages/chap02/src/main/webapp/index.jsp
@@ -1,5 +1,7 @@
 <html>
-<body>
-<h2>Hello World!</h2>
+  <body>
+    $foreach(model.times)
+    <h2>${model.title}</h2>
+    $foreachend
 </body>
 </html>
