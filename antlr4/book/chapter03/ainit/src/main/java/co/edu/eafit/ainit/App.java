ffe7ce808b5cff40d9d3625641550e9bb857823d
Juan Francisco Cardona McCormick <jfcmacro@gmail.com>
2025-07-05 19:46:58 +0000

    Updating antlr4

--- antlr4/book/chapter03/ainit/src/main/java/co/edu/eafit/ainit/App.java
+++ antlr4/book/chapter03/ainit/src/main/java/co/edu/eafit/ainit/App.java
@@ -4,10 +4,8 @@ package co.edu.eafit.ainit;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 }
