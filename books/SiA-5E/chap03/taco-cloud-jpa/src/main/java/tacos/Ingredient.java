6a320bc158f7bf969dfc7b227691b3fcc079e259
Juan Francisco Cardona McCormick <jfcmacro@gmail.com>
2025-07-24 20:15:29 +0000

    Adding Examples with update version Spring 6.0

--- books/SiA-5E/chap03/taco-cloud-jpa/src/main/java/tacos/Ingredient.java
+++ books/SiA-5E/chap03/taco-cloud-jpa/src/main/java/tacos/Ingredient.java
@@ -1,8 +1,7 @@
 package tacos;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.domain.Persistable;
-import org.springframework.data.relational.core.mapping.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -10,22 +9,17 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Table
+@Entity
 @AllArgsConstructor
 @NoArgsConstructor(access=AccessLevel.PRIVATE, force=true)
-public class Ingredient implements Persistable<String> {
+public class Ingredient {
 
     @Id
     private String id;
     
     private String name;
     private Type type;
-
-    @Override
-    public boolean isNew() {
-	return true;
-    }
-
+    
     public enum Type {
 	WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
     }
