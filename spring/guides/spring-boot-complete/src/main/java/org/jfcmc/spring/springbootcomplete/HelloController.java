74bb4709a2c826c1fe9cf64cceb841071bceea8c
Juan Francisco Cardona McCormick <Juan_Cardona@epam.com>
2026-03-16 11:39:42 -0500

    Adicionando Spring Boot Guide "Building and Application with Spring Boot"

---
+++ spring/guides/spring-boot-complete/src/main/java/org/jfcmc/spring/springbootcomplete/HelloController.java
@@ -0,0 +1,12 @@
+package org.jfcmc.spring.springbootcomplete;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String index() {
+	return "Greetings from Spring Boot!";
+    }
+}
---
+++ spring/guides/spring-boot-complete/src/test/java/org/jfcmc/spring/springbootcomplete/SpringBootCompleteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jfcmc.spring.springbootcomplete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCompleteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
